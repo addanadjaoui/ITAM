be5a4bc1432a7dbbddc2b9a04e136d198e5f6db0
addanadjaoui <44801474+addanadjaoui@users.noreply.github.com>
2026-02-04 10:02:47 +0100

    Update AssetList.js implement "const API = "http://localhost:8000/api";"

--- frontend/src/components/AssetList.js
+++ frontend/src/components/AssetList.js
@@ -4,7 +4,7 @@ export default function AssetList() {
   const [assets, setAssets] = useState([]);
 
   useEffect(() => {
-    fetch("http://192.168.56.110:8000/assets")
+    fetch("http://192.168.56.110:8000/api/assets")
       .then((res) => res.json())
       .then(setAssets);
   }, []);
