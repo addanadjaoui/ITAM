75fa3995c35c7b88df3aba04639e7cdb568f27a5
addanadjaoui <nadjaouiadda@gmail.com>
2026-02-04 09:26:28 +0000

    Local changes to DiscoverForm

--- frontend/src/components/DiscoverForm.js
+++ frontend/src/components/DiscoverForm.js
@@ -3,15 +3,27 @@ import React, { useEffect, useState } from "react";
 export default function App() {
   const [assets, setAssets] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [ip_address, setIp] = useState("");
 
   const loadAssets = () => {
+<<<<<<< HEAD
     fetch("http://192.168.56.110:8000/api/assets")
       .then(r => r.json())
+=======
+    fetch("http://192.168.56.110:8000/assets")
+      .then((r) => r.json())
+>>>>>>> 2013531 (Local changes to DiscoverForm)
       .then(setAssets);
   };
 
   const discoverAD = async () => {
+<<<<<<< HEAD
     await fetch("http://192.168.56.110:8000/api/discover?mode=ad", { method: "POST" });
+=======
+    await fetch("http://192.168.56.110:8000/discover?mode=ad", {
+      method: "POST",
+    });
+>>>>>>> 2013531 (Local changes to DiscoverForm)
     loadAssets();
   };
 
@@ -28,7 +40,7 @@ export default function App() {
   const discoverNow = async () => {
     setLoading(true);
     const res = await fetch("http://192.168.56.110:8000/discover", {
-      method: "POST"
+      method: "POST",
     });
     const data = await res.json();
     alert(`Découverte terminée : ${data.discovered} assets`);
@@ -39,16 +51,20 @@ export default function App() {
   useEffect(loadAssets, []);
 
   return (
-    <div style={{padding:20}}>
+    <div className="actions">
+      <input
+        placeholder="IP"
+        value={ip_address}
+        onChange={(e) => setIp(e.target.value)}
+      />
 
-      <button onClick={discoverNow} disabled={loading}>
-        {loading ? "Découverte en cours..." : "Découvrir les assets (AD)"}
-      </button>
+      <div className="buttons">
+        <button onClick={discoverNow} disabled={loading}>
+          {loading ? "Découverte en cours..." : "Découvrir les assets (AD)"}
+        </button>
 
-      <button onClick={discoverAD}>Découverte AD</button>
-      <button onClick={discoverNetwork}>Découverte Réseau</button>
-
+        <button onClick={discoverNetwork}>Découverte Réseau</button>
+      </div>
     </div>
   );
 }
-
